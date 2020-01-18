185f551edf053f5ea1ff70467751f450d8b9c24a
Jarosław Pawlak <jaroslaw.pawlak@gmail.com>
2020-01-18 15:04:03 +0100

    Initial Commit

---
+++ loyaltycards/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  loyaltycards
+//
+//  Created by Jarosław Pawlak on 18/01/2020.
+//  Copyright © 2020 MAJATECH Jarosław Pawlak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
